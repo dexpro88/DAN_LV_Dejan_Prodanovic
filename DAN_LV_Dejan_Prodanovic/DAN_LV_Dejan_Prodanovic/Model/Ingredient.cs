bed62875d6ae616dcf80bce04d2a6fb06034e0a5
dexpro988 <dejan.prodanovic@panonit.com>
2020-08-23 23:55:17 -0700

    Project created

---
+++ DAN_LV_Dejan_Prodanovic/DAN_LV_Dejan_Prodanovic/Model/Ingredient.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAN_LV_Dejan_Prodanovic.Model
+{
+    class Ingredient
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+    }
+}
---
+++ DAN_LV_Dejan_Prodanovic/DAN_LV_Dejan_Prodanovic/Model/Pizza.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAN_LV_Dejan_Prodanovic.Model
+{
+    class Pizza
+    {
+        public string Size { get; set; }
+        public List<Ingredient> Ingredients { get; set; }
+
+        public Pizza()
+        {
+            Ingredients = new List<Ingredient>();
+        }
+    }
+}
